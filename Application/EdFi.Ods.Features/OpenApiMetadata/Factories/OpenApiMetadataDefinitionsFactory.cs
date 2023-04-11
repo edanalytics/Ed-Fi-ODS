14a83b35b7aaf14f2d893d24f88b568d388b4118
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-04-10 20:31:12 -0500

    Fix the Open API metadata for required embedded objects (affecting at least the top-level embedded object of an extension).

--- Application/EdFi.Ods.Features/OpenApiMetadata/Factories/OpenApiMetadataDefinitionsFactory.cs
+++ Application/EdFi.Ods.Features/OpenApiMetadata/Factories/OpenApiMetadataDefinitionsFactory.cs
@@ -156,7 +156,7 @@ namespace EdFi.Ods.Features.OpenApiMetadata.Factories
                     resourceChildItem.EmbeddedObjects.Select(
                         e => new
                         {
-                            IsRequired = false,
+                            IsRequired = e.Association.IsRequired,
                             Key = e.JsonPropertyName,
                             Schema = CreateEmbeddedObjectSchema(e, openApiMetadataResource)
                         })).ToList();
