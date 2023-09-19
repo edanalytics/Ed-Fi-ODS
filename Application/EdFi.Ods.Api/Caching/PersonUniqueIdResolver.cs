8a2bf432109f066dbe1448fabc4dbe6ee91f4789
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-09-19 02:23:46 -0500

    Added a missing UniqueId resolution step while performing Updates to an existing entity.

--- Application/EdFi.Ods.Api/Caching/PersonUniqueIdResolver.cs
+++ Application/EdFi.Ods.Api/Caching/PersonUniqueIdResolver.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EdFi.Ods.Api.IdentityValueMappers;
+using EdFi.Ods.Common.Caching;
 using EdFi.Ods.Common.Providers;
 
 namespace EdFi.Ods.Api.Caching;
